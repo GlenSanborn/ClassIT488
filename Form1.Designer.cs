9c24df73e34a760fe680979054ee20d84464d0bc
glen sanborn <glennewmansanborn@yahoo.com>
2021-05-09 11:23:27 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace SocialMaxPC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.FBUserName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.FBPassWord = new System.Windows.Forms.TextBox();
+            this.FBNickName = new System.Windows.Forms.TextBox();
+            this.TUserName = new System.Windows.Forms.TextBox();
+            this.TPassWord = new System.Windows.Forms.TextBox();
+            this.TNickName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(295, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Post Message";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(313, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(327, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "View Past Posted Messages";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // FBUserName
+            // 
+            this.FBUserName.Location = new System.Drawing.Point(38, 188);
+            this.FBUserName.Name = "FBUserName";
+            this.FBUserName.Size = new System.Drawing.Size(240, 20);
+            this.FBUserName.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(131, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "FaceBook";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(447, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Twitter";
+            // 
+            // FBPassWord
+            // 
+            this.FBPassWord.Location = new System.Drawing.Point(38, 282);
+            this.FBPassWord.Name = "FBPassWord";
+            this.FBPassWord.Size = new System.Drawing.Size(240, 20);
+            this.FBPassWord.TabIndex = 5;
+            // 
+            // FBNickName
+            // 
+            this.FBNickName.Location = new System.Drawing.Point(38, 379);
+            this.FBNickName.Name = "FBNickName";
+            this.FBNickName.Size = new System.Drawing.Size(240, 20);
+            this.FBNickName.TabIndex = 6;
+            // 
+            // TUserName
+            // 
+            this.TUserName.Location = new System.Drawing.Point(329, 188);
+            this.TUserName.Name = "TUserName";
+            this.TUserName.Size = new System.Drawing.Size(267, 20);
+            this.TUserName.TabIndex = 7;
+            // 
+            // TPassWord
+            // 
+            this.TPassWord.Location = new System.Drawing.Point(329, 282);
+            this.TPassWord.Name = "TPassWord";
+            this.TPassWord.Size = new System.Drawing.Size(267, 20);
+            this.TPassWord.TabIndex = 8;
+            // 
+            // TNickName
+            // 
+            this.TNickName.Location = new System.Drawing.Point(329, 379);
+            this.TNickName.Name = "TNickName";
+            this.TNickName.Size = new System.Drawing.Size(267, 20);
+            this.TNickName.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(97, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Please Enter User Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(104, 236);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Please Enter Password";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(86, 331);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(154, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Please Enter Account Moniker ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(406, 150);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(123, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Please Enter User Name";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(406, 236);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(116, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Please Enter Password";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(388, 331);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(145, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Please Enter Accout Moniker";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(75, 438);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(165, 23);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Save FaceBook Credential ";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(388, 438);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(141, 23);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Save Twitter Credential";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 574);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TNickName);
+            this.Controls.Add(this.TPassWord);
+            this.Controls.Add(this.TUserName);
+            this.Controls.Add(this.FBNickName);
+            this.Controls.Add(this.FBPassWord);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FBUserName);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox FBUserName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox FBPassWord;
+        private System.Windows.Forms.TextBox FBNickName;
+        private System.Windows.Forms.TextBox TUserName;
+        private System.Windows.Forms.TextBox TPassWord;
+        private System.Windows.Forms.TextBox TNickName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
